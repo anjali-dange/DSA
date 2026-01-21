3a9b105cd2109282a76602d716820b5581df7933
anjali-dange <anjalidange9370@gmail.com>
2026-01-21 23:43:38 +0530

    Create Selection_Sort.java

---
+++ Selection_Sort.java
@@ -0,0 +1,27 @@
+package dsa;
+public class Selection_Sort{
+	static void selection_sort(int arr[]) {
+		int n=arr.length;
+		for(int i=0;i<n-1;i++) {
+			int min_idx=i;
+			for(int j=i+1;j<n;j++) {
+				if(arr[min_idx]>arr[j]) {
+					min_idx=j;
+				}
+			}
+			int temp=arr[i];
+			arr[i]=arr[min_idx];
+			arr[min_idx]=temp;
+		}
+	}
+	
+	public static void main(String[] args) {
+			int arr[]= {10,3,6,2,5,9};
+			selection_sort(arr);
+			for(int i=0;i<arr.length;i++) {
+				System.out.println(arr[i]);
+			}
+	
+		
+	}
+}
