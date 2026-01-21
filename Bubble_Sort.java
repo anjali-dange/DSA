5921566519c028c9372912c82c4fa6576fd6c59f
anjali-dange <anjalidange9370@gmail.com>
2026-01-21 23:41:28 +0530

    Update Bubble_Sort.java

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -1,25 +1,23 @@
 package dsa;
-public class Bubble_Sort {
-    public static void main(String[] args) {
-        int arr[] = {5, 3, 4, 2, 1, 7};
-        int n = arr.length;
-        for (int i = n - 1; i > 0; i--) {
-            boolean isswap = false;
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    isswap = true;
-                }
-            }
-            if (!isswap) {
-                break;
-            }
-        }
-        System.out.println("The sorted array is:");
-        for (int s = 0; s < n; s++) {
-            System.out.print(arr[s] + " ");
-        }
-    }
+public class Bubble_Sort{
+	static void bubble_sort(int arr[]) {
+	int n=arr.length;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n-i;j++) {
+			if(arr[j]>arr[j+1]) {
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	}
+	public static void main(String[] args) {
+		int arr[]= {5,3,8,4,2};
+		System.out.println("after swapping");
+		bubble_sort(arr);
+		for(int x:arr) {
+			System.out.println(x+"  ");
+		}
+	}
 }
